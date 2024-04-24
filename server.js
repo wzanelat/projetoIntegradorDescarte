d6f5cf40e756df0b66dcf0760b79e7348c57930e
WellingtonZanelatto <wellingtonzanelatto@gmail.com>
2024-04-24 20:57:45 -0300

    commit 240420242057

--- server.js
+++ server.js
@@ -8,15 +8,14 @@ const PORT = process.env.PORT
 
 app.use(bodyParser.json())
 app.use('/api', api)
-
-console.log(process.env.MONGO_PASS)
-
+//
+//
 if(process.env.NODE_ENV  === 'production'){
     app.use(express.static('frontend/build'))
 
     const path = require('path')
     app.get('*', (req,res)=>{
-        res.sendFile(path.resolve(__dirname, "frontend", 'build', 'index.html'))
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
     })
 }
 
